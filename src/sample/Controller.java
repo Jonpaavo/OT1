127489a58bc6faad9f1e2e96f7e21773e74186dc
aatupal <aatupal@uef.fi>
2021-04-23 15:44:19 +0300

    updeittiä

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.Objects;
+
+
 public class Controller {
+    public void button(ActionEvent actionEvent) throws Exception {
+        Parent root1 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("tokanaytto.fxml")));
+        Scene kehys = new Scene(root1);
+        Stage ikkuna = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        ikkuna.setScene(kehys);
+        ikkuna.show();
+
+        }
 }
+
