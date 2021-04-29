def9ff7b25a5cb7b925c201ecba91243da890d2b
asserpik <83001485+asserpik@users.noreply.github.com>
2021-04-29 12:03:29 +0300

    fxml päivitys

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.InputMethodEvent;
 
 import java.util.*;
@@ -13,6 +14,7 @@ public class Controller {
     // Luodaan tarvittavat elementit ja listat
     public ComboBox cboxToimintaalue;
     public ComboBox cboxHenkilomaara;
+    public TextField tftoimintaalue;
     public ObservableList<String>toimintaaluelista = FXCollections.observableArrayList("Tahko", "Ruka", "Ylläs", "Himos", "Levi", "Koli", "Vuokatti", "Pallas");
     public ObservableList<String>henkilomaaralista = FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10","yli 10");
 
@@ -35,6 +37,10 @@ public class Controller {
         System.out.println(cboxToimintaalue.getSelectionModel().getSelectedItem());
         System.out.println(cboxHenkilomaara.getSelectionModel().getSelectedItem());
 
+    }
+
+    public void btTallenna(){
+        System.out.println(tftoimintaalue.getCharacters());
     }
 
 
