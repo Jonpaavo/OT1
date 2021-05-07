2445d781c60fa517e900b764a0b72a18b8f25109
MinnaPilto <minnapilto@gmail.com>
2021-05-07 14:51:02 +0300

    .

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,11 +9,9 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.text.Text;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
 
 public class Controller {
 
@@ -41,6 +39,119 @@ public class Controller {
     public ObservableList<String>vuokattimokitlista = FXCollections.observableArrayList("Vuokatti 1", "Vuokatti 2", "Vuokatti 3", "Vuokatti 4", "Vuokatti 5");
     public ObservableList<String>pallasmokitlista = FXCollections.observableArrayList("Pallas 1", "Pallas 2", "Pallas 3", "Pallas 4", "Pallas 5");
     public Text txtVarausVaroitus;
+
+    //MÖKINVARAUS TABLE
+    public TableView <Varaus> tvTableView;
+    public TableColumn<Varaus, String> tcToimintaalue;
+    public TableColumn<Varaus, String> tcTulopaiva;
+    public  TableColumn<Varaus, String> tcLahtopaiva;
+    public  TableColumn<Varaus, Integer> tcHenkilomaara;
+    public  TableColumn<Varaus, String> tcAsiakas;
+    public  TableColumn<Varaus, String> tcPalvelut;
+    public  TableColumn<Varaus, String> tcMokki;
+    // MÖKKIENHALLINTA
+    //Mökkienhallinta table
+    public TableColumn <Mokki, String> CmokkienhallintaNimi;
+    public TableColumn <Mokki, String> CmokkienhallintaOsoite;
+    public TableColumn <Mokki, String>CmokkienhallintaKuvaus;
+    public TableColumn <Mokki, String>CmokkienhallintaHlomaara;
+    public TableColumn <Mokki, String>CmokkienhallintaVarustelu;
+    public TableColumn <Mokki, String>CmokkienhallintaPostinro;
+    public TableColumn <Mokki, Double> CmokkienhallintaHinta;
+    public TableColumn <Mokki, Double> CmokkienhallintaAlv;
+    public TableColumn <Mokki, Integer>CmokkienhallintaID;
+    public TableView tbvMokkienhallintaMokit;
+
+    public TextField txtfMokkienhallintaMokkinimi;
+    public TextField txtfMokkienhallintaKatuosoite;
+    public TextField txtfMokkienhallintaKuvaus;
+    public TextField txtfMokkienhallintaHlomaara;
+    public TextField txtfMokkienhallintaVarustelu;
+    public TextField txtfMokkienhallintaPostinumero;
+    public TextField txtfMokkienhallintaHinta;
+    public TextField txtfMokkienhallintaAlv;
+    public TextField txtfMokkienhallintaMokkiID;
+
+    // ASIAKASHALLINTA alustus
+    public TextField tfEtunimi;
+    public TextField tfSukunimi;
+    public TextField tfLahiosoite;
+    public TextField tfPostinro;
+    public TextField tfEmail;
+    public TextField tfPuhelinnro;
+    public TableView tbvAsiakas;
+    public TableColumn tbcEtunimi;
+    public TableColumn tbcSukunimi;
+    public TableColumn tbcLahiosoite;
+    public TableColumn tbcPostinro;
+    public TableColumn tbcEmail;
+    public TableColumn tbcPuhelinnro;
+
+    // Näyttää toiminta-alueet Palveluiden Hallinta - välilehdellä
+    public void NaytaToimintaalue3() {
+        cboxToimintaalue3.setItems(toimintaaluelista);
+    }
+
+    // Näyttää toiminta-alueet Mökkien Hallinta - välilehdellä
+    public void NaytaToimintaalue2() {
+        cboxToimintaalue2.setItems(toimintaaluelista);
+    }
+
+    // Lisätään toiminta-alueet comboboxiin
+    public void NaytaToimintaalue() {
+        cboxToimintaalue.setItems(toimintaaluelista);
+    }
+
+    // Lisätään henkilömäärät comboboxiin
+    public void NaytaHenkilomaara(){
+        cboxHenkilomaara.setItems(henkilomaaralista);
+    }
+
+    public void NaytaPalvelut(){
+        cboxPalvelut.setItems(palvelulista);
+    }
+    public void mokkinimi(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void osoite(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void postinro(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void hlomaara(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void kuvaus(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void hinta(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void alv(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void varustelu(InputMethodEvent inputMethodEvent) {
+    }
+
+    public void tallennaPalvelu(ActionEvent actionEvent) {
+    }
+
+    public void tallennaAsiakas(ActionEvent actionEvent) {
+    }
+    public void initialize() {
+        NaytaToimintaalue();
+        NaytaToimintaalue2();
+        NaytaToimintaalue3();
+        NaytaHenkilomaara();
+        NaytaPalvelut();
+        NaytaMokki();
+        Varoitus();
+    }
+
+    /**
+     * MÖKINVARAUSNÄKYMÄ
+     */
 
     // Antaa valittavaksi mökit siltä toimialueelta, joka näytöllä on valittuna
     public void NaytaMokki() {
@@ -72,16 +183,6 @@ public class Controller {
     public void Varoitus() {
         txtVarausVaroitus.setVisible(false);
     }
-
-    //MÖKINVARAUS TABLE
-    public TableView <Varaus> tvTableView;
-    public TableColumn<Varaus, String> tcToimintaalue;
-    public TableColumn<Varaus, String> tcTulopaiva;
-    public  TableColumn<Varaus, String> tcLahtopaiva;
-    public  TableColumn<Varaus, Integer> tcHenkilomaara;
-    public  TableColumn<Varaus, String> tcAsiakas;
-    public  TableColumn<Varaus, String> tcPalvelut;
-    public  TableColumn<Varaus, String> tcMokki;
 
     //lista johon menee kaikki Varaus-oliot
     ObservableList<Varaus> tilausLista = FXCollections.observableArrayList();
@@ -119,40 +220,76 @@ public class Controller {
         }
     }
 
-    // MÖKKIENHALLINTA
-    public TableColumn CmokkienhallintaNimi;
-    public TableColumn CmokkienhallintaOsoite;
-    public TableColumn CmokkienhallintaKuvaus;
-    public TableColumn CmokkienhallintaHlomaara;
-    public TableColumn CmokkienhallintaVarustelu;
-    public TableColumn CmokkienhallintaPostinro;
-    public TableColumn CmokkienhallintaHinta;
-    public TableColumn CmokkienhallintaAlv;
-    public TableColumn CmokkienhallintaID;
-    public TableView tbvMokkienhallintaMokit;
-    public TextField txtfMokkienhallintaMokkinimi;
-    public TextField txtfMokkienhallintaKatuosoite;
-    public TextField txtfMokkienhallintaKuvaus;
-    public TextField txtfMokkienhallintaHlomaara;
-    public TextField txtfMokkienhallintaVarustelu;
-    public TextField txtfMokkienhallintaPostinumero;
-    public TextField txtfMokkienhallintaHinta;
-    public TextField txtfMokkienhallintaAlv;
-    // ASIAKASHALLINTA alustus
-    public TextField tfEtunimi;
-    public TextField tfSukunimi;
-    public TextField tfLahiosoite;
-    public TextField tfPostinro;
-    public TextField tfEmail;
-    public TextField tfPuhelinnro;
-    public TableView tbvAsiakas;
-    public TableColumn tbcEtunimi;
-    public TableColumn tbcSukunimi;
-    public TableColumn tbcLahiosoite;
-    public TableColumn tbcPostinro;
-    public TableColumn tbcEmail;
-    public TableColumn tbcPuhelinnro;
+    /**
+     * MÖKKIENHALLINTANÄKYMÄ
+     */
 
+    //Lisätään mökille toiminta-alue ID
+
+    public int HaeToimintaAlueid() {
+        int toimintaAlueId;
+        String toimintaAlueNimi = cboxToimintaalue2.getValue().toString();
+
+        if (toimintaAlueNimi.equals("TAHKO"))
+            toimintaAlueId = 1;
+        else if (toimintaAlueNimi.equals("RUKA"))
+            toimintaAlueId = 2;
+        else if (toimintaAlueNimi.equals("YLLÄS"))
+            toimintaAlueId = 3;
+        else if (toimintaAlueNimi.equals("HIMOS"))
+            toimintaAlueId = 4;
+        else if (toimintaAlueNimi.equals("LEVI"))
+            toimintaAlueId = 5;
+        else if (toimintaAlueNimi.equals("KOLI"))
+            toimintaAlueId = 6;
+        else if (toimintaAlueNimi.equals("VUOKATTI"))
+            toimintaAlueId = 7;
+        else if (toimintaAlueNimi.equals("PALLAS"))
+            toimintaAlueId = 8;
+        else
+            toimintaAlueId = 0;
+
+        return toimintaAlueId;
+    }
+    //Lisätään lista, johon lisätään kaikki mökki-oliot
+    ObservableList<Mokki> mokit = FXCollections.observableArrayList();
+
+    // Lisää mökkien tiedot mökkinäytön tauluun
+    public void LisaaMokki() {
+
+        //muuttujat teksikenttien sisällöstä
+
+        int mokkiID = Integer.parseInt(txtfMokkienhallintaMokkiID.getText());
+        String mokkinimi = txtfMokkienhallintaMokkinimi.getText();
+        String katuosoite = txtfMokkienhallintaKatuosoite.getText();
+        String kuvaus = txtfMokkienhallintaKuvaus.getText();
+        int henkilomaara = Integer.parseInt(txtfMokkienhallintaHlomaara.getText());
+        String varustelu = txtfMokkienhallintaVarustelu.getText();
+        String postinro = txtfMokkienhallintaPostinumero.getText();
+        double hinta = Double.parseDouble(txtfMokkienhallintaHinta.getText());
+        double alv = Double.parseDouble(txtfMokkienhallintaAlv.getText());
+
+        //lisätään taulun sarakkeisiin
+        CmokkienhallintaNimi.setCellValueFactory(new PropertyValueFactory<Mokki, String>("mokkinimi"));
+        CmokkienhallintaOsoite.setCellValueFactory(new PropertyValueFactory<Mokki, String>("katuosoite"));
+        CmokkienhallintaKuvaus.setCellValueFactory(new PropertyValueFactory<Mokki, String>("kuvaus"));
+        CmokkienhallintaHlomaara.setCellValueFactory(new PropertyValueFactory<Mokki, String>("henkilomaara"));
+        CmokkienhallintaVarustelu.setCellValueFactory(new PropertyValueFactory<Mokki, String>("varustelu"));
+        CmokkienhallintaPostinro.setCellValueFactory(new PropertyValueFactory<Mokki, String>("postinro"));
+        CmokkienhallintaHinta.setCellValueFactory(new PropertyValueFactory<Mokki, Double>("hinta"));
+        CmokkienhallintaAlv.setCellValueFactory(new PropertyValueFactory<Mokki, Double>("alv"));
+        CmokkienhallintaID.setCellValueFactory(new PropertyValueFactory<Mokki, Integer>("mokkiID"));
+
+
+        // lisätään mökki olio, joka lisäätän mökkilistalle
+        mokit.add(new Mokki(mokkiID, HaeToimintaAlueid(), postinro, mokkinimi, katuosoite, kuvaus,
+                henkilomaara, varustelu, hinta, alv));
+
+        //lisätään mökkilista taululle
+
+        tbvMokkienhallintaMokit.setItems(mokit);
+
+    }
     // ASIAKASHALLINTA SQL
     // Asiakastaulu
 
@@ -171,7 +308,6 @@ public class Controller {
         }
         return;
     }
-
 
     //Asiakkaiden lisääminen sql asiakas tauluun
     public void BtTallennaAsiakas() {
@@ -207,132 +343,4 @@ public class Controller {
         }
     }
 
-
-
-    public void initialize() {
-        NaytaToimintaalue();
-        NaytaToimintaalue2();
-        NaytaToimintaalue3();
-        NaytaHenkilomaara();
-        NaytaPalvelut();
-        NaytaMokki();
-        Varoitus();
-
-
-
-    }
-    // Näyttää toiminta-alueet Palveluiden Hallinta - välilehdellä
-    public void NaytaToimintaalue3() {
-        cboxToimintaalue3.setItems(toimintaaluelista);
-    }
-
-    // Näyttää toiminta-alueet Mökkien Hallinta - välilehdellä
-    public void NaytaToimintaalue2() {
-        cboxToimintaalue2.setItems(toimintaaluelista);
-    }
-
-
-
-    // Lisätään toiminta-alueet comboboxiin
-    public void NaytaToimintaalue() {
-        cboxToimintaalue.setItems(toimintaaluelista);
-    }
-
-    // Lisätään henkilömäärät comboboxiin
-    public void NaytaHenkilomaara(){
-        cboxHenkilomaara.setItems(henkilomaaralista);
-    }
-
-    public void NaytaPalvelut(){
-        cboxPalvelut.setItems(palvelulista);
-    }
-
-
-
-
-
-    /**
-     * Lisätään mökille toiminta-alue
-     *
-     * @return
-     */
-    public int HaeToimintaAlueid() {
-        int toimintaAlueId;
-        String toimintaAlueNimi = cboxToimintaalue2.getValue().toString();
-
-        if (toimintaAlueNimi.equals("TAHKO"))
-            toimintaAlueId = 1;
-        else if (toimintaAlueNimi.equals("RUKA"))
-            toimintaAlueId = 2;
-        else if (toimintaAlueNimi.equals("YLLÄS"))
-            toimintaAlueId = 3;
-        else if (toimintaAlueNimi.equals("HIMOS"))
-            toimintaAlueId = 4;
-        else if (toimintaAlueNimi.equals("LEVI"))
-            toimintaAlueId = 5;
-        else if (toimintaAlueNimi.equals("KOLI"))
-            toimintaAlueId = 6;
-        else if (toimintaAlueNimi.equals("VUOKATTI"))
-            toimintaAlueId = 7;
-        else if (toimintaAlueNimi.equals("PALLAS"))
-            toimintaAlueId = 8;
-        else
-            toimintaAlueId = 0;
-
-        return toimintaAlueId;
-    }
-
-    /**
-     * Mökkienhallinta-välilehden Tallenna-painikkeen toiminnallisuus
-     *
-     * @throws SQLException
-     */
-
-
-    public void LisaaMokki()  {
-        String nimi = txtfMokkienhallintaMokkinimi.getText();
-        String osoite = txtfMokkienhallintaKatuosoite.getText();
-        String kuvaus = txtfMokkienhallintaKuvaus.getText();
-        int hmaara = Integer.parseInt(txtfMokkienhallintaHlomaara.getText());
-        String varustelu = txtfMokkienhallintaVarustelu.getText();
-        String postinumero = txtfMokkienhallintaPostinumero.getText();
-        double hinta = Double.parseDouble(txtfMokkienhallintaHinta.getText());
-        double alv = Double.parseDouble(txtfMokkienhallintaAlv.getText());
-
-        Mokki mokki = new Mokki(0, HaeToimintaAlueid(), postinumero, nimi,
-                osoite, kuvaus, hmaara, varustelu, hinta,alv);
-
-    }
-
-
-
-    public void mokkinimi(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void osoite(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void postinro(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void hlomaara(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void kuvaus(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void hinta(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void alv(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void varustelu(InputMethodEvent inputMethodEvent) {
-    }
-
-    public void tallennaPalvelu(ActionEvent actionEvent) {
-    }
-
-    public void tallennaAsiakas(ActionEvent actionEvent) {
-    }
 }
